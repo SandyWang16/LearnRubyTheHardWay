6b0b206703a9025c4b65b7f5d5373f81de14e40e
sihhan <thehappyend95@gmail.com>
2018-03-21 22:34:12 +0800

    inital commit

---
+++ ex3.rb
@@ -0,0 +1,28 @@
+# 印出 "I will now count my chickens:"
+puts "I will now count my chickens:"
+#印出 Hens 並印出 25 + 30 / 6 的結果
+puts "Hens", 25 + 30 / 6
+#印出 Roosters 並印出  100 - 25 * 3 % 4 (%取餘數)
+puts "Roosters", 100 - 25 * 3 % 4
+# 印出 "Now I will count the eggs:"
+puts "Now I will count the eggs:"
+# 印出計算結果
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+#印出 Is it true that 3 + 2 < 5 - 7?"
+puts "Is it true that 3 + 2 < 5 - 7?"
+# 印出 3 + 2 < 5 - 7 的比較結果 回傳true or false
+puts 3 + 2 < 5 - 7
+# 印出 "What is 3 + 2?" 並印出算出結果
+puts "What is 3 + 2?", 3 + 2
+# 印出 "What is 5 - 7" 並印出算出結果
+puts "What is 5 - 7?", 5 - 7
+# 印出"Oh, that's why it's false."
+puts "Oh, that's why it's false."
+# 印出"How about some more."
+puts "How about some more."
+#印出"Is it greater?" 並印出 5 > -2 的比較結果 回傳true or false
+puts "Is it greater?", 5 > -2
+#印出"Is it greater or equal?" 並印出 5 >= -2的比較結果 回傳true or false
+puts "Is it greater or equal?", 5 >= -2
+#印出""Is it less or equal?" 並印出  5 <= -2 的比較結果 回傳true or false
+puts "Is it less or equal?", 5 <= -2
