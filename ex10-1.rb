d9c54c8d95d0f27567da0d0ccad77a42e831b9c4
sihhan <thehappyend95@gmail.com>
2018-03-28 23:31:39 +0800

    ex10

---
+++ ex10-1.rb
@@ -0,0 +1,24 @@
+#\'	Single-quote (') 單引號
+puts 'It\'s a single-quote.'
+#\"	Double-quote (") 雙引號
+puts " I'm \" Double-quote \". "
+#\a	ASCII bell (BEL) 警示音
+puts "\a It will have sound \a"
+#\b	ASCII backspace (BS) 刪除
+puts "Hello\b\b\b\b\bGoodbye world"
+#\f	ASCII formfeed (FF) 換頁
+puts " i'm \fnext."
+#\n	ASCII linefeed (LF) 換行
+puts "1. Hello\n2. World"
+#\r ASCII Carriage Return (CR)   歸位符號
+puts "Hello\rStart over world"
+#\t ASCII Horizontal Tab (TAB)   Tab鍵
+puts "Hello\tworld"
+#\uxxxx Character with 16-bit hex value xxxx (Unicode only) 16進制數字的字符串表示形式
+puts " It's unicode '\u5b57'."
+#\v ASCII vertical tab (VT)  垂直定位
+puts " it can vertical tab \vwow."
+#\ooo Character with octal value ooo  八進位數字的 Unicode 符號
+puts "It's Character with octal value ooo '\170' ."
+#\xhh Character with hex value hh  十六進位數字的 Unicode 符號
+puts "It's Character with hex value hh ' \x6A' ."
